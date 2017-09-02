db36eac362041fdd5a1abe631bc8ed08bd0a7321
Dabin Kim <dabin0450@naver.com>
2017-09-02 11:59:25 +0900

    Add files via upload

---
+++ day1/exampl2.py
@@ -0,0 +1,11 @@
+num1=int(input("입력:"))
+if num1>=90:
+    print("A")
+elif num1>=80:
+    print("B")
+elif num1 >= 70:
+    print("C")
+elif num1 >= 60:
+    print("D")
+else:
+    print("F")
---
+++ day1/if_example.py
@@ -0,0 +1,12 @@
+math_score=100
+english_score=100
+
+# if (a==b && b==c)
+if math_score >=90 and english_score >=90:
+    print('Perfect')
+
+math_score=100
+english_score=50
+# if (a==b || b==c)
+if math_score >=80 or english_score >=90:
+    print('Oh')
